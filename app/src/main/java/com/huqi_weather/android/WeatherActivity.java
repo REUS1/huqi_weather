bca046ed80c0ac357313d03dccf2062b4ab4ebbc
REUS1 <724097145@qq.com>
2017-07-11 01:01:58 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/huqi_weather/android/WeatherActivity.java
+++ app/src/main/java/com/huqi_weather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.huqi_weather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.huqi_weather.android.gson.Forecast;
 import com.huqi_weather.android.gson.Weather;
+import com.huqi_weather.android.service.AutoUpdateService;
 import com.huqi_weather.android.util.HttpUtil;
 import com.huqi_weather.android.util.Utility;
 
@@ -184,6 +186,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE );
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
     /**
      * 加载必应每日一图
